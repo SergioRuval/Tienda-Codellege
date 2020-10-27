03799d7353513d7f16b56dd8fbb7eb22a36453c9
SergioRuval <ruvaloser@gmail.com>
2020-10-26 19:38:38 -0600

    Tienda en línea creada con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+//Rutas
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent}
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
